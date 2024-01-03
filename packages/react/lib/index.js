78886c1690781dad2ab15399eff78f02d72b77c1
Reactongraph <js.developer@ongraph.com>
2024-01-03 18:53:19 +0530

    remove lib from react

--- packages/react/lib/index.js
+++
@@ -1,3 +0,0 @@
-export { B as Button } from './Button-LUnlYWN6.js';
-import 'react';
-//# sourceMappingURL=index.js.map
--- packages/react/lib/index2.js
+++
@@ -1,3 +0,0 @@
-export { B as default } from './Button-LUnlYWN6.js';
-import 'react';
-//# sourceMappingURL=index2.js.map
